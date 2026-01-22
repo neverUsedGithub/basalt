a0483ecddb6d2a7acc975cb2e68456227b20db4d
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2026-01-22 19:18:28 +0100

    switch to @types/bun from bun-types

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -12,13 +12,6 @@ import { CodeClientConnection } from "../codeclient";
 import { BlockSplitter } from "../splitter";
 import { compileProject } from "../basalt/compileProject";
 
-// const dec = await decompressDF(
-//   JSON.parse(
-//     '{"author":"CodeClient","name":"Template to be placed","version":1,"code":"H4sIAAAAAAAACr2UQWvcMBSE/4qZa33otmnZPOgh9FbYHtqSSwhCK70YJbJkLHlpMf7vQbYFiVk3S5bk6GfN+H0zwj321quHALrpYTRoekYJqaLxDoQf3jiU85zAB3YxvW+rAOphItdJfjsM5cIiS5S0Vtx1Th2VldAyShD0nZWuEsaZiKVXXofw4Vt2BiFwFAfZLn0nmsj1OEk206HpQz2crBmEe2+cUL4beYLyTRoGeWCNYSgRrI+gj+MqT71cVy+9CJssiaDNcGIYM3iIrXHVKyEu9YWS++32s9Z8sc0gIFjj+L8cr81kBTA3hLcoSOSMzsQ7Na33IlTsIreshW9G71P5ktTXzZrhqW2B8Jud3nEIsuJMBUJj5T9uxXzqJbaVXdKfo/ie6qPinJv4Uvm0WTpib0WUVTalfg1sLYckJlxZU7maXSx2Xqfx3BLhF1edHbc+yNbIveUze84woE9f34jmD/+NxbW0HRc7bqsp0UyEn74IjVQcjjEd79d5MUue7f8l3bfb4RGiF67sWwYAAA=="}',
-//   ).code,
-// );
-// await Bun.write("debug-blocks-parsed.json", JSON.stringify(dec, null, 2));
-
 if (Bun.argv.length < 3) {
   logger.fail("usage: basalt <project>");
   process.exit(1);
